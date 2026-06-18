c8b12203d086dbd8ffa12ac6c79f7cdded96159f
indelgarcia <indelg7@gmail.com>
2026-06-18 17:07:00 -0500

    update .gitignore to include .env file; update tableau to have unque incident counts

--- streamlit-app/main_v3.py
+++ streamlit-app/main_v3.py
@@ -15,128 +15,152 @@ import duckdb
 # =============================================================
 
 TABLEAU_HTML_CATEGORY = """
-<div class='tableauPlaceholder' id='viz1755837857221' style='position: relative'>
-    <noscript>
-        <a href='#'>
-            <img alt='Dashboard 1 ' src='https://public.tableau.com/static/images/In/Incidentspercategorytogetherwithpercentageandseriousv_non-serious/Dashboard1/1_rss.png' style='border: none' />
-        </a>
-    </noscript>
-    <object class='tableauViz' style='display:none;'>
-        <param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' />
-        <param name='embed_code_version' value='3' />
-        <param name='site_root' value='' />
-        <param name='name' value='Incidentspercategorytogetherwithpercentageandseriousv_non-serious/Dashboard1' />
-        <param name='tabs' value='no' />
-        <param name='toolbar' value='yes' />
-        <param name='static_image' value='https://public.tableau.com/static/images/In/Incidentspercategorytogetherwithpercentageandseriousv_non-serious/Dashboard1/1.png' />
-        <param name='animate_transition' value='yes' />
-        <param name='display_static_image' value='yes' />
-        <param name='display_spinner' value='yes' />
-        <param name='display_overlay' value='yes' />
-        <param name='display_count' value='yes' />
-        <param name='language' value='en-US' />
-    </object>
+<div class='tableauPlaceholder' id='viz1781820161402' style='position: relative'>
+  <noscript>
+    <a href='#'>
+      <img
+        alt='Dashboard 1 '
+        src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;In&#47;Incidentspercategorytogetherwithpercentageandseriousv_non-serious&#47;Dashboard1&#47;1_rss.png'
+        style='border: none'
+      />
+    </a>
+  </noscript>
+
+  <object class='tableauViz' style='display:none;'>
+    <param name='host_url'             value='https%3A%2F%2Fpublic.tableau.com%2F' />
+    <param name='embed_code_version'   value='3' />
+    <param name='site_root'            value='' />
+    <param name='name'                 value='Incidentspercategorytogetherwithpercentageandseriousv_non-serious&#47;Dashboard1' />
+    <param name='tabs'                 value='no' />
+    <param name='toolbar'              value='yes' />
+    <param name='static_image'         value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;In&#47;Incidentspercategorytogetherwithpercentageandseriousv_non-serious&#47;Dashboard1&#47;1.png' />
+    <param name='animate_transition'   value='yes' />
+    <param name='display_static_image' value='yes' />
+    <param name='display_spinner'      value='yes' />
+    <param name='display_overlay'      value='yes' />
+    <param name='display_count'        value='yes' />
+    <param name='language'             value='en-US' />
+  </object>
 </div>
+
 <script type='text/javascript'>
-    var divElement = document.getElementById('viz1755837857221');
-    var vizElement = divElement.getElementsByTagName('object')[0];
-    if (divElement.offsetWidth > 800) {
-        vizElement.style.width='100%';
-        vizElement.style.height='827px';
-    } else if (divElement.offsetWidth > 500) {
-        vizElement.style.width='100%';
-        vizElement.style.height='827px';
-    } else {
-        vizElement.style.width='100%';
-        vizElement.style.height='1127px';
-    }
-    var scriptElement = document.createElement('script');
-    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
-    vizElement.parentNode.insertBefore(scriptElement, vizElement);
+  var divElement = document.getElementById('viz1781820161402');
+  var vizElement = divElement.getElementsByTagName('object')[0];
+
+  if (divElement.offsetWidth > 800) {
+    vizElement.style.width  = '100%';
+    vizElement.style.height = '827px';
+  } else if (divElement.offsetWidth > 500) {
+    vizElement.style.width  = '100%';
+    vizElement.style.height = '827px';
+  } else {
+    vizElement.style.width  = '100%';
+    vizElement.style.height = '1127px';
+  }
+
+  var scriptElement = document.createElement('script');
+  scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
+  vizElement.parentNode.insertBefore(scriptElement, vizElement);
 </script>
 """
 
 TABLEAU_HTML_YEAR = """
-<div class='tableauPlaceholder' id='viz1755839651562' style='position: relative'>
-    <noscript>
-        <a href='#'>
-            <img alt='Dashboard 1 ' src='https://public.tableau.com/static/images/In/IncidentsPerYear/Dashboard1/1_rss.png' style='border: none' />
-        </a>
-    </noscript>
-    <object class='tableauViz' style='display:none;'>
-        <param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' />
-        <param name='embed_code_version' value='3' />
-        <param name='site_root' value='' />
-        <param name='name' value='IncidentsPerYear/Dashboard1' />
-        <param name='tabs' value='no' />
-        <param name='toolbar' value='yes' />
-        <param name='static_image' value='https://public.tableau.com/static/images/In/IncidentsPerYear/Dashboard1/1.png' />
-        <param name='animate_transition' value='yes' />
-        <param name='display_static_image' value='yes' />
-        <param name='display_spinner' value='yes' />
-        <param name='display_overlay' value='yes' />
-        <param name='display_count' value='yes' />
-        <param name='language' value='en-US' />
-    </object>
+<div class='tableauPlaceholder' id='viz1781819740999' style='position: relative'>
+  <noscript>
+    <a href='#'>
+      <img
+        alt='Dashboard 1 '
+        src='https://public.tableau.com/static/images/In/IncidentsPerYear/Dashboard1/1_rss.png'
+        style='border: none'
+      />
+    </a>
+  </noscript>
+
+  <object class='tableauViz' style='display:none;'>
+    <param name='host_url'             value='https%3A%2F%2Fpublic.tableau.com%2F' />
+    <param name='embed_code_version'   value='3' />
+    <param name='site_root'            value='' />
+    <param name='name'                 value='IncidentsPerYear/Dashboard1' />
+    <param name='tabs'                 value='no' />
+    <param name='toolbar'              value='yes' />
+    <param name='static_image'         value='https://public.tableau.com/static/images/In/IncidentsPerYear/Dashboard1/1.png' />
+    <param name='animate_transition'   value='yes' />
+    <param name='display_static_image' value='yes' />
+    <param name='display_spinner'      value='yes' />
+    <param name='display_overlay'      value='yes' />
+    <param name='display_count'        value='yes' />
+    <param name='language'             value='en-US' />
+  </object>
 </div>
+
 <script type='text/javascript'>
-    var divElement = document.getElementById('viz1755839651562');
-    var vizElement = divElement.getElementsByTagName('object')[0];
-    if (divElement.offsetWidth > 800) {
-        vizElement.style.width='100%';
-        vizElement.style.height='827px';
-    } else if (divElement.offsetWidth > 500) {
-        vizElement.style.width='100%';
-        vizElement.style.height='827px';
-    } else {
-        vizElement.style.width='100%';
-        vizElement.style.height='877px';
-    }
-    var scriptElement = document.createElement('script');
-    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
-    vizElement.parentNode.insertBefore(scriptElement, vizElement);
+  var divElement = document.getElementById('viz1781819740999');
+  var vizElement = divElement.getElementsByTagName('object')[0];
+
+  if (divElement.offsetWidth > 800) {
+    vizElement.style.width  = '100%';
+    vizElement.style.height = '827px';
+  } else if (divElement.offsetWidth > 500) {
+    vizElement.style.width  = '100%';
+    vizElement.style.height = '827px';
+  } else {
+    vizElement.style.width  = '100%';
+    vizElement.style.height = '877px';
+  }
+
+  var scriptElement = document.createElement('script');
+  scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
+  vizElement.parentNode.insertBefore(scriptElement, vizElement);
 </script>
 """
 
 TABLEAU_HTML_MONTH = """
-<div class='tableauPlaceholder' id='viz1755839865249' style='position: relative'>
-    <noscript>
-        <a href='#'>
-            <img alt='Dashboard 1 ' src='https://public.tableau.com/static/images/In/IncidentsPerMonth/Dashboard1/1_rss.png' style='border: none' />
-        </a>
-    </noscript>
-    <object class='tableauViz' style='display:none;'>
-        <param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' />
-        <param name='embed_code_version' value='3' />
-        <param name='site_root' value='' />
-        <param name='name' value='IncidentsPerMonth/Dashboard1' />
-        <param name='tabs' value='no' />
-        <param name='toolbar' value='yes' />
-        <param name='static_image' value='https://public.tableau.com/static/images/In/IncidentsPerMonth/Dashboard1/1.png' />
-        <param name='animate_transition' value='yes' />
-        <param name='display_static_image' value='yes' />
-        <param name='display_spinner' value='yes' />
-        <param name='display_overlay' value='yes' />
-        <param name='display_count' value='yes' />
-        <param name='language' value='en-US' />
-    </object>
+<div class='tableauPlaceholder' id='viz1781819053747' style='position: relative'>
+  <noscript>
+    <a href='#'>
+      <img
+        alt='Dashboard 1 '
+        src='https://public.tableau.com/static/images/In/IncidentsPerMonth/Dashboard1/1_rss.png'
+        style='border: none'
+      />
+    </a>
+  </noscript>
+
+  <object class='tableauViz' style='display:none;'>
+    <param name='host_url'             value='https%3A%2F%2Fpublic.tableau.com%2F' />
+    <param name='embed_code_version'   value='3' />
+    <param name='site_root'            value='' />
+    <param name='name'                 value='IncidentsPerMonth/Dashboard1' />
+    <param name='tabs'                 value='no' />
+    <param name='toolbar'              value='yes' />
+    <param name='static_image'         value='https://public.tableau.com/static/images/In/IncidentsPerMonth/Dashboard1/1.png' />
+    <param name='animate_transition'   value='yes' />
+    <param name='display_static_image' value='yes' />
+    <param name='display_spinner'      value='yes' />
+    <param name='display_overlay'      value='yes' />
+    <param name='display_count'        value='yes' />
+    <param name='language'             value='en-US' />
+  </object>
 </div>
+
 <script type='text/javascript'>
-    var divElement = document.getElementById('viz1755839865249');
-    var vizElement = divElement.getElementsByTagName('object')[0];
-    if (divElement.offsetWidth > 800) {
-        vizElement.style.width='100%';
-        vizElement.style.height='827px';
-    } else if (divElement.offsetWidth > 500) {
-        vizElement.style.width='100%';
-        vizElement.style.height='827px';
-    } else {
-        vizElement.style.width='100%';
-        vizElement.style.height='877px';
-    }
-    var scriptElement = document.createElement('script');
-    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
-    vizElement.parentNode.insertBefore(scriptElement, vizElement);
+  var divElement = document.getElementById('viz1781819053747');
+  var vizElement = divElement.getElementsByTagName('object')[0];
+
+  if (divElement.offsetWidth > 800) {
+    vizElement.style.width  = '100%';
+    vizElement.style.height = '827px';
+  } else if (divElement.offsetWidth > 500) {
+    vizElement.style.width  = '100%';
+    vizElement.style.height = '827px';
+  } else {
+    vizElement.style.width  = '100%';
+    vizElement.style.height = '877px';
+  }
+
+  var scriptElement = document.createElement('script');
+  scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
+  vizElement.parentNode.insertBefore(scriptElement, vizElement);
 </script>
 """
 
